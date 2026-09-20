9b5c585b075baa32bfb2a67b85975f3536a2d4f5
Minh <58528323+duyminh2420@users.noreply.github.com>
2026-09-19 21:27:32 -0400

    Time: 1335 ms (20.45%) | Memory: 20.1 MB (88.64%) - LeetSync

---
+++ 2420-closest-node-to-path-in-tree/closest-node-to-path-in-tree.py
@@ -0,0 +1,31 @@
+class Solution:
+    def closestNode(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+        
+        def function(start, end):
+            stack = [(start, -1, [start])]
+        
+            while stack:
+                node, parent, path = stack.pop()
+
+                if node == end:
+                    return path
+                for neighbor in graph[node]:
+                    if neighbor != parent:
+                        stack.append([neighbor, node, path + [neighbor]])
+            return []
+        def kunction(node, path):
+            stack, visited, path_set = [node], {node}, set(path)
+            while stack:
+                nd = stack.pop(0)
+                if nd in path_set:
+                    return nd
+                
+                for neighbor in graph[nd]:
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+                        visited.add(neighbor)
+        return [kunction(k, function(i,j)) for i,j,k in query]
