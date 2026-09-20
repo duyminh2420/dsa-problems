5d2d0b7d0f197979ef76d5694b2e562837a02f72
Minh <58528323+duyminh2420@users.noreply.github.com>
2026-09-19 21:21:28 -0400

    Time: 154 ms (78.52%) | Memory: 56.3 MB (57.72%) - LeetSync

---
+++ 2485-finding-the-number-of-visible-mountains/finding-the-number-of-visible-mountains.py
@@ -0,0 +1,16 @@
+class Solution:
+    def visibleMountains(self, peaks: List[List[int]]) -> int:
+        n = len(peaks)
+
+        #sort by x-intercept of left mountain ascending first and x-intercept(x+y) of right mountain descending
+        peaks.sort(key = lambda x: (x[0] - x[1], -(x[0] + x[1])))
+        count = 0
+        maxEnd = -inf
+        #while interating peaks, count the number of visible peak which has larger x of the right mountain 
+        for i, (x,y) in enumerate(peaks):
+            if x + y > maxEnd:
+                maxEnd = x + y
+                #consider dup
+                if i < n - 1 and peaks[i] == peaks[i+1]: continue
+                count += 1
+        return count 
